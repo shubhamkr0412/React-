197aafe095863f4a1a3568992011d08f7108131a
Shubham Kumar <shubham35godda@gmail.com>
2022-03-27 01:45:25 +0530

    added menu data

---
+++ app1/src/Components/Basics/MenuCard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const MenuCard = ({menuData}) => {
+   
+  return (
+    <>
+    <section className='main-card--cointainer'>
+    {menuData.map((currElem)=>{
+     return(<div className="card-container " key={currElem.id}>
+    <div className="card">
+      <div className="card-body">
+        <span className="card-number card-circle subtle">{currElem.id}</span>
+      <span className="card-author subtle" > {currElem.name}</span>
+      <h2 className="card-title">{currElem.name}</h2>
+<span className="card-description subtle">{currElem.description}</span>
+   <div className="card-read">Read</div>
+
+      </div>
+      <img src={currElem.image} alt="images" className="card-media"/>
+<span className="card-tag subtle">Order now</span>
+    </div>
+  </div>)
+    })}
+    </section>
+  </>
+  )
+}
+
+export default MenuCard
