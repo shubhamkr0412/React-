0bbcfa7e8c8f2682917181ce3e1173e2d1000187
Shubham Kumar <shubham35godda@gmail.com>
2022-03-28 00:27:13 +0530

    Created navbar and added filter functionality

--- app1/src/Components/Basics/Restaurant.js
+++ app1/src/Components/Basics/Restaurant.js
@@ -3,10 +3,27 @@ import "./style.css";
 import Menu from "./menuApi"
 import { useState } from "react";
 import MenuCard from "./MenuCard";
+
 const Restaurant = () => {
     const [menuData,setMenuData]=useState(Menu);
+    const filterItem=(category)=>{
+      const updatedList=Menu.filter((currElem)=>{
+        return currElem.category==category;
+      });
+      setMenuData(updatedList) ;
+}
   return (
     <>
+    <nav className="navbar">
+<div className="btn-group">
+  <button className="btn-group__item" onClick={()=>filterItem("breakfast")}>Breakfast</button>
+  <button className="btn-group__item" onClick={()=>filterItem("lunch")}>Lunch</button>
+  <button className="btn-group__item" onClick={()=>filterItem("evening")}>Evening</button>
+  <button className="btn-group__item" onClick={()=>filterItem("dinner")}>Dinner</button>
+  <button className="btn-group__item" onClick={()=>setMenuData(Menu)}>All</button>
+  
+</div>
+    </nav>
      <MenuCard menuData={menuData} />
     </>
   );
